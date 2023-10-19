a1569677f2486b684587fd95c9192f8dbc09cdc2
Krish Goel <krishgoel20mar@gmail.com>
2023-10-19 21:45:08 +0530

    Changes made.

--- src/components/Database.js
+++ src/components/Database.js
@@ -7,18 +7,19 @@ const app = express();
 
 // models/User.js
 const userSchema = new mongoose.Schema({
-  User_Email_ID: String,
-  Password: String, // Store hashed passwords
+  Username: String,
+  Password: String,
+  Email_ID: String, // Store hashed passwords
 });
 
 module.exports = mongoose.model('User', userSchema);
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:3000/User-Registration-System', {
+mongoose.connect('mongodb://localhost:3000/User_Registration_System', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
+
 // Registration endpoint - Registering a new user
 app.post('/register', async (req, res) => {
   try 
@@ -36,9 +37,9 @@ app.post('/register', async (req, res) => {
 // server.js
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:3000/User-Registration-System';
+const uri = 'mongodb://localhost:3000/User_Registration_System';
 
-async function watchUserRegistrations() {
+async function insert() {
   const client = new MongoClient(uri, { useUnifiedTopology: true });
 
   try 
@@ -64,4 +65,116 @@ async function watchUserRegistrations() {
   }
 }
 
-watchUserRegistrations();
+insert();
+
+async function deletion() {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+  try
+  {
+    await client.connect();
+    const db = client.db();
+    const collection = db.collection('users');
+
+    const changeStream = collection.watch();
+
+    changeStream.on('change', (change) => {
+      if (change.operationType === 'deletion')
+      {
+        // A new user has been registered; can perform updates here
+        console.log('New user registered:', change.fullDocument);
+        // Perform update operations here
+      }
+    });
+  } 
+  catch (error) 
+  {
+    console.error('Error watching for changes:', error);
+  }
+}
+
+deletion();
+
+async function read() {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+  try 
+  {
+    await client.connect();
+    const db = client.db();
+    const collection = db.collection('users');
+
+    const changeStream = collection.watch();
+
+    changeStream.on('change', (change) => {
+      if (change.operationType === 'insert') 
+      {
+        // A new user has been registered; can perform updates here
+        console.log('New user registered:', change.fullDocument);
+        // Perform update operations here
+      }
+    });
+  } 
+  catch (error) 
+  {
+    console.error('Error watching for changes:', error);
+  }
+}
+
+read();
+
+async function update() {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+  try 
+  {
+    await client.connect();
+    const db = client.db();
+    const collection = db.collection('users');
+
+    const changeStream = collection.watch();
+
+    changeStream.on('change', (change) => {
+      if (change.operationType === 'insert') 
+      {
+        // A new user has been registered; can perform updates here
+        console.log('New user registered:', change.fullDocument);
+        // Perform update operations here
+      }
+    });
+  } 
+  catch (error) 
+  {
+    console.error('Error watching for changes:', error);
+  }
+}
+
+update();
+
+async function search() {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+  try 
+  {
+    await client.connect();
+    const db = client.db();
+    const collection = db.collection('users');
+
+    const changeStream = collection.watch();
+
+    changeStream.on('change', (change) => {
+      if (change.operationType === 'insert') 
+      {
+        // A new user has been registered; can perform updates here
+        console.log('New user registered:', change.fullDocument);
+        // Perform update operations here
+      }
+    });
+  } 
+  catch (error) 
+  {
+    console.error('Error watching for changes:', error);
+  }
+}
+
+search();
